4f81454cd445a79fc4313991139aa07ea07ad4b2
Christoper <c.blake@hotmail.se>
2021-11-05 08:34:18 +0100

    //9. Create 1:20 https://www.youtube.com/watch?v=WWimijQqteY Create File(s) inside folder at once.         // Add new file Extension => extension manager shift+f2         // file1.cs, file2.cs or IMapper => interface.

--- MachineFuncApi/Extensions/Mapper.cs
+++ MachineFuncApi/Extensions/Mapper.cs
@@ -1,12 +1,18 @@
 ﻿using MachineFuncApi.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace MachineFuncApi
+namespace MachineFuncApi.Extensions
 {
     public static class Mapper //8. Map machine to and From Table Entity 77
     {
+        // Table storage is a very cheap storage. XBOX live is on table storage.
+        //9. Create 1:20 https://www.youtube.com/watch?v=WWimijQqteY Create File(s) inside folder at once.
+        // Add new file Extension => extension manager shift+f2
+        // file1.cs, file2.cs or IMapper => interface.
         public static MachineTableEntity ToTableEntity(this Machine machine)
         {
             return new MachineTableEntity
