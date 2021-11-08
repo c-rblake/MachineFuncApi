4745b310b3e70416c99ee68c7b18a578fc65fbcc
Christoper <c.blake@hotmail.se>
2021-11-08 16:39:50 +0100

    Added the share project to the razor project

---
+++ MachineParkRazor/Pages/MachineOverview.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineParkRazor.Pages
+{
+    public partial class MachineOverview : ComponentBase
+    {
+        
+    }
+}
